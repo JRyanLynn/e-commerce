a12c3e50dbd93edef436e89096528849591a9bdd
jryanlynn <jryanlynn@gmail.com>
2022-10-27 17:03:06 -0700

    committing slider and announcement banner

---
+++ src/data.js
@@ -0,0 +1,27 @@
+export const sliderItems = [
+
+{
+    id: 1,
+    img: 'https://i.ibb.co/cXFnLLV/3.png',
+    title: 'FALL SALE',
+    desc: 'WARM UP TO COOL FASHION! GET 20% OFF OF ALL NEW GEAR',
+    bg: 'f5fafd',
+},
+
+{
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: 'GET INVOLVED',
+    desc: 'THIS SALE IS IN TENTS! SCOPE OUT BACKPACKING GEAR STARTING AT 10% OFF',
+    bg: 'fcf1ed',
+},
+
+{
+    id: 3,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: 'LOVE THE CRISP AIR',
+    desc: 'GET YOUR BIKING GEAR FOR A FRACTION OF THE PRICE!',
+    bg: 'fbf0f4',
+}
+
+]
