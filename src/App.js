85c85e2c40743023eec24913f83a9d8cf7ef1615
jryanlynn <jryanlynn@gmail.com>
2022-10-28 18:40:53 -0700

    Login and register page done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Product from './Pages/Product';
 import Home from './Pages/Home';
 import ProductList from './Pages/ProductList';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
 
 
 
 const App  = () => {
   return (
-    <Product />
+    <Login />
   )
 }
 
