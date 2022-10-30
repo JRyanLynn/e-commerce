12a1792bcbfeb7b86a661a6eb6d01487681fa028
jryanlynn <jryanlynn@gmail.com>
2022-10-30 16:53:41 -0700

    shopping cart done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import Home from './Pages/Home';
 import ProductList from './Pages/ProductList';
 import Register from './Pages/Register';
 import Login from './Pages/Login';
+import Cart from './Pages/Cart';
 
 
 
 const App  = () => {
   return (
-    <Login />
+    <Cart />
   )
 }
 
